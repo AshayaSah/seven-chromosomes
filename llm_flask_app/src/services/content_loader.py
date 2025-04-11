348622b2dcdf1f19b470980cc4d1f5f4ee617735
rabindra-basnet <120715649+rabindra-basnet@users.noreply.github.com>
2025-04-12 01:54:41 +0545

    added the multiple file at a same time to upload content_loader.py

--- llm_flask_app/src/services/content_loader.py
+++ llm_flask_app/src/services/content_loader.py
@@ -1,28 +1,42 @@
-from typing import List
+from typing import List, Union
 from langchain_community.document_loaders import PyPDFLoader, WebBaseLoader, TextLoader
 from langchain_core.documents import Document
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 
-def load_content(source: str, source_type: str) -> List[Document]:
+
+
+def load_content(source: Union[str, List[str]], source_type: str) -> List[Document]:
     try:
+        # Ensure sources is a list
+        sources = [source] if isinstance(source, str) else source
+        if not isinstance(sources, list):
+            raise ValueError("Source must be a string or list of strings")
+        documents = []
         if source_type == "pdf":
-            loader = PyPDFLoader(source)
-            return loader.load()
+            for src in sources:
+                loader = PyPDFLoader(src)
+                documents.extend(loader.load())
         elif source_type == "web":
-            # if isinstance(source, list):
-            #     loader = WebBaseLoader(source)
-            # else:
-            loader = WebBaseLoader([source])
-            return loader.load()
+            loader = WebBaseLoader(
+                sources
+            )
+            documents.extend(loader.load())
         elif source_type == "text":
-            loader = TextLoader(source)
-            return loader.load()
+            for src in sources:
+                loader = TextLoader(src)
+                documents.extend(loader.load())
         elif source_type == "raw":
-            return [Document(page_content=source, metadata={"source": "raw_input"})]
+            for src in sources:
+                documents.append(
+                    Document(page_content=src, metadata={"source": "raw_input"})
+                )
         else:
             raise ValueError(f"Unsupported source type: {source_type}")
+        return documents
+
     except Exception as e:
         raise Exception(f"Error loading content: {e}")
+
 
 def get_text_chunks(documents: List[Document]) -> List[str]:
     text_splitter = RecursiveCharacterTextSplitter(chunk_size=10000, chunk_overlap=1000)
