bec89fe8920a0f9a1248330b257bc9f6c46cd641
Rabindra Basnet <rabindrabasnet82@gmail.com>
2025-04-12 14:53:44 +0545

    added cors features

--- llm_flask_app/app.py
+++ llm_flask_app/app.py
@@ -1,12 +1,19 @@
 from flask import Flask
+from flask_cors import CORS
 from src.utils.logger import setup_logger
 from src.routes.api_routes import api_bp
-from config import USER_AGENT, HOST,DEBUG
+from src.config import USER_AGENT, HOST, DEBUG
+from dotenv import load_dotenv
 
+load_dotenv()
 
 
 def create_app():
     app = Flask(__name__)
+    # CORS(app)
+    CORS(app, origins=["*"], 
+        methods=["GET", "POST"], 
+        allow_headers=["Content-Type", "Authorization"])
     logger = setup_logger()
     logger.info("Starting LLM Flask application")
     app.register_blueprint(api_bp, url_prefix="/api")
@@ -15,5 +22,5 @@ def create_app():
 
 if __name__ == "__main__":
     app: Flask = create_app()
-    USER_AGENT=USER_AGENT
-    app.run(debug=DEBUG, host=HOST, load_dotenv=True)
+    USER_AGENT = USER_AGENT
+    app.run(debug=DEBUG, host=HOST)
