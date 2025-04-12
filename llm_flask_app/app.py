f93b43f1cbc79acc75275e46abb4c122d3cd426f
Rabindra Basnet <rabindrabasnet82@gmail.com>
2025-04-12 07:50:32 +0545

    added useragent for webbasedloader

--- llm_flask_app/app.py
+++ llm_flask_app/app.py
@@ -1,17 +1,19 @@
 from flask import Flask
 from src.utils.logger import setup_logger
 from src.routes.api_routes import api_bp
-from config import DEBUG, HOST
+from config import USER_AGENT, HOST,DEBUG
+
+
 
 def create_app():
     app = Flask(__name__)
     logger = setup_logger()
     logger.info("Starting LLM Flask application")
-
     app.register_blueprint(api_bp, url_prefix="/api")
     return app
 
 
 if __name__ == "__main__":
     app: Flask = create_app()
-    app.run(debug=DEBUG, host=HOST, load_dotenv = True)
+    USER_AGENT=USER_AGENT
+    app.run(debug=DEBUG, host=HOST, load_dotenv=True)
